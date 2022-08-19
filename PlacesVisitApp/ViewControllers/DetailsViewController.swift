732f9cd22c8d6d7934861d1d64f48ff0f1f5221e
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-08-19 23:05:56 +0300

    Project architecture created.

---
+++ PlacesVisitApp/ViewControllers/DetailsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailsViewController.swift
+//  PlacesVisitApp
+//
+//  Created by Mustafa Çiçek on 19.08.2022.
+//
+
+import UIKit
+import MapKit
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var detailsImageView: UIImageView!
+    @IBOutlet weak var detailsNameLabel: UILabel!
+    @IBOutlet weak var detailsTypeLabel: UILabel!
+    @IBOutlet weak var detailsAtmosphereLabel: UILabel!
+    @IBOutlet weak var mapKitView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
